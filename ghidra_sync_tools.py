2ae6d623b967a418a8695e07c0af03f3207284e6
Your Name <you@example.com>
2025-04-20 21:30:31 +0200

    a

--- ghidra_sync_tools.py
+++ ghidra_sync_tools.py
@@ -1,10 +1,13 @@
 ﻿from ghidra_bridge import GhidraBridge
 from time import sleep
+
+
 def connect():
     print("[*] Connecting to GhidraBridge...")
     bridge = GhidraBridge()  # no namespace injection
     print("[+] Connected.")
     return bridge
+
 
 def get_functions(bridge):
     result = bridge.remote_eval(
@@ -17,7 +20,28 @@ def get_functions(bridge):
         functions[name] = addr
     return functions
 
+
+def execute_in_transaction(bridge, statement: str, transaction_name: str):
+    return bridge.remote_exec(f"""
+tx = currentProgram.startTransaction("{transaction_name}")
+try:
+{statement}
+finally:
+    currentProgram.endTransaction(tx, True)
+""")
+
+
 def highlight_instruction(bridge, addr_hex):
+    execute_in_transaction(bridge,
+                           f"""
+    addr = toAddr("{addr_hex}")
+    goTo(addr)
+""",
+                           transaction_name="SyncAddress")
+    print(f"[+] Jumped to {addr_hex} and tagged it.")
+
+
+def change_color_at_addr(bridge, addr_hex, color):
     script = f"""
 tx = currentProgram.startTransaction("SyncHighlight")
 
@@ -29,7 +53,6 @@ finally:
 """
     bridge.remote_exec(script)
     print(f"[+] Jumped to {addr_hex} and tagged it.")
-
 
 
 if __name__ == "__main__":
